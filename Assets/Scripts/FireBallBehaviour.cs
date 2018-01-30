0cba88236b442c15c8eaee879078e3d1b9d8d268
Unknown <asser.salmela@gmail.com>
2018-01-30 15:45:40 +0200

    Hieman parempi tuliballo
    asdfg

---
+++ Assets/Scripts/FireBallBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallBehaviour : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public GameObject childBall;
+    public bool canExplode;
+    public bool canMultiply;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!canExplode)
+            return;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Instantiate(explosionPrefab, pos, rot);
+
+        if (canMultiply)
+        {
+            Instantiate(childBall, pos, rot);
+        }
+        Destroy(gameObject);
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
