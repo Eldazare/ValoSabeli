c9218e204832bf1f4909f2042fd089f53f2e3961
Unknown <asser.salmela@gmail.com>
2018-01-31 16:15:50 +0200

    Fixed Fireball
    Spawnaa pienempiä tuliefektejä ja muuttujat lisätty. Materiaali vielä ruma -> saa muuttaa vapaasti

--- Assets/Scripts/FireBallBehaviour.cs
+++ Assets/Scripts/FireBallBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Effects;
 
 public class FireBallBehaviour : MonoBehaviour
 {
@@ -8,19 +9,35 @@ public class FireBallBehaviour : MonoBehaviour
     public GameObject childBall;
     public bool canExplode;
     public bool canMultiply;
+    public int numberOfSpawns;
+    public int expForce;
+    public float spawnXZRandRange;
+    public float spawnYRandRange;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (!canExplode)
             return;
+
+        if (collision.collider.CompareTag("FireBall"))
+            return;
+
         ContactPoint contact = collision.contacts[0];
         Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
         Vector3 pos = contact.point;
-        Instantiate(explosionPrefab, pos, rot);
+        GameObject explosion;
+        explosionPrefab.GetComponent<ExplosionPhysicsForce>().explosionForce = expForce;
+        explosion = Instantiate(explosionPrefab, pos, rot);
+        
 
         if (canMultiply)
         {
-            Instantiate(childBall, pos, rot);
+            for (int i = 0; i < numberOfSpawns; i++) {
+                Vector3 position = new Vector3(Random.Range(-spawnXZRandRange, spawnXZRandRange), Random.Range(-0.3f, spawnYRandRange), Random.Range(-spawnXZRandRange, spawnXZRandRange));
+                GameObject tuhottava = Instantiate(childBall, pos + position, rot);
+
+                Destroy(tuhottava, 8f);
+            }
         }
         Destroy(gameObject);
     }
