b5d420cbb9480113e5b93314f9cccd5cfa2074e1
JimiJokela1 <jimi.jokela@metropolia.fi>
2018-02-05 12:32:18 +0200

    Scale and animations

---
+++ Assets/Scripts/CharAnimations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharAnimations : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnInteractableObjectGrabbed(GameObject obj)
+	{
+		animator.SetBool("Ragdolled", true);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Ground")
+		{
+			animator.SetBool("Ragdolled", false);
+			animator.SetTrigger("Land");
+		}
+	}
+}
